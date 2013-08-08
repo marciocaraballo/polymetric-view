375ae0468d9e5bac0d8a4d8941606343d4fa11ac
Marcio Caraballo <marcio.caraballososa@gmail.com>
2013-08-08 00:30:46 -0300

    Added dep from d3.chart on d3 in shim

--- examples/js/main.js
+++ examples/js/main.js
@@ -13,13 +13,14 @@ require.config({
 		'd3'						:			'vendor/d3/d3',
 		'd3.chart'			:			'vendor/d3.chart/d3.chart',
 		'underscore'		:			'vendor/underscore/underscore'
-		
+
 	},
 	shim : {
 		'd3' : {
 			exports : 'd3'
 		},
 		'd3.chart' : {
+			deps : ['d3'],
 			exports : 'd3'
 		},
 		'underscore' : {
@@ -31,4 +32,6 @@ require.config({
 requirejs(['d3','underscore','d3.chart'],
 function (d3, _){
 	console.log('Lets get this started!');
+
+	var root = d3.select('#root');
 });
