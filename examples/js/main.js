47b80a264050e163d49c3559fb3a405284c0f7d5
Marcio Caraballo <marcio.caraballososa@gmail.com>
2013-08-08 00:15:09 -0300

    Added initial project structure

---
+++ examples/js/main.js
@@ -0,0 +1,32 @@
+/**
+Main application example
+
+Proyect will draw a Polymetric View
+
+@author "Marcio Caraballo <marcio.caraballososa@gmail.com>"
+*/
+require.config({
+	baseUrl : '../',
+	paths : {
+		/** Libraries */
+		'd3'						:			'vendor/d3/d3',
+		'd3.chart'			:			'vendor/d3.chart/d3.chart',
+		'underscore'		:			'vendor/underscore/underscore'
+	},
+	shim : {
+		'd3' : {
+			exports : 'd3'
+		},
+		'd3.chart' : {
+			exports : 'd3'
+		},
+		'underscore' : {
+			exports : '_'
+		}
+	}
+});
+
+requirejs(['d3','underscore','d3.chart'],
+function (d3, _){
+	console.log('Lets get this started!');
+});
